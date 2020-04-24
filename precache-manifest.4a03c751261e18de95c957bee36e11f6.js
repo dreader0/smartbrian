0736cddc5aba853b69dd3e1c1318e964b5624a54
Dan <danreader0@gmail.com>
2020-04-24 18:34:41 -0400

    Updates

---
+++ precache-manifest.4a03c751261e18de95c957bee36e11f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd52a396192939cc7dee73a203aa686",
+    "url": "/smartbrian/index.html"
+  },
+  {
+    "revision": "577ff844f5274b165a2e",
+    "url": "/smartbrian/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "4f4476c2e71a463b0c38",
+    "url": "/smartbrian/static/css/main.aa16afc2.chunk.css"
+  },
+  {
+    "revision": "577ff844f5274b165a2e",
+    "url": "/smartbrian/static/js/2.78af93ba.chunk.js"
+  },
+  {
+    "revision": "4f4476c2e71a463b0c38",
+    "url": "/smartbrian/static/js/main.50463570.chunk.js"
+  },
+  {
+    "revision": "16c2a96d99e8321abfe9",
+    "url": "/smartbrian/static/js/runtime-main.eae530f9.js"
+  }
+]);
